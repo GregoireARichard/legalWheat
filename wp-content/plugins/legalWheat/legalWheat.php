2158d9109860ea65bca5e63bc0054065addd0893
Grégoire <gregoire.richard@hetic.net>
2021-06-06 00:47:28 +0200

    added plug-in (not totally working)

--- wp-content/plugins/legalWheat/legalWheat.php
+++ wp-content/plugins/legalWheat/legalWheat.php
@@ -4,12 +4,13 @@ Plugin Name: legalWheat Handler
 Plugin URI: https://github.com/kokorirko/
 Description: Plug-in pour gérer les différentes options de legalWheat et garder son nez hors de la farine
 Version: 0.0.1
-Author : SuperGreggo
+Author: SuperGreggo
 Author URI: https://github.com/kokorirko/
 Text Domain: Legal Wheeeat
 
 
 */ 
+require_once plugin_dir_path(__FILE__) . 'includes/lw-function.php';
 if(!defined('ABSPATH')){
     die;
 }
@@ -23,7 +24,7 @@ function what_a_week_huh(){ // captain it's wednesday !!
 
 function mfp_Add_Text(){
     echo "<div style='width:100%; background-color:#211F20; margin:0 auto; text-align:center;' >";
-    echo "<p style = 'color : #FDFCF9;'>Attention ! Certaines farines sont... spéciales :)</p>";
+    echo "<p style = 'color : #FDFCF9;'>Attention ! Certaines farines sont... spéciales. Serez vous les repérer?</p>";
     echo "</div>";
 }
 
