1b5b27d16d098c4b270f3d081d980e5f8c4f5966
Grégoire <gregoire.richard@hetic.net>
2021-06-05 16:09:02 +0200

    Started plug-in

--- wp-content/plugins/legalWheat/legalWheat.php
+++ wp-content/plugins/legalWheat/legalWheat.php
@@ -14,10 +14,25 @@ if(!defined('ABSPATH')){
     die;
 }
 
-function change_admin_footer($text){
-    $text = "Merci de contribuer à l'agrandissement de legalWheat";
-    return $text;
-
+function what_a_week_huh(){ // captain it's wednesday !!
+    if(date("l") === "Wednesday"){
+        echo "<p style = 'color : black;'>What a week, huh?</p>";
+        echo "<p style = 'color : black;'>- Captain it's Wednesday !</p>";
+    }
 }
-add_filter('admin_footer_text', 'change_admin_footer');
+
+function mfp_Add_Text(){
+    echo "<div style='width:100%; background-color:#211F20; margin:0 auto; text-align:center;' >";
+    echo "<p style = 'color : #FDFCF9;'>Attention ! Certaines farines sont... spéciales :)</p>";
+    echo "</div>";
+}
+
+function prevent_db_space($the_Post){
+    $the_New_Post = str_replace("  ", " ",$the_Post);
+    return $the_New_Post;
+}
+add_action("wp_footer","mfp_Add_Text");
+add_action("wp_footer", "what_a_week_huh");
+add_action("the_content", "prevent_db_space");
+
 ?>
